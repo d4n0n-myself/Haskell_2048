474b97d72952dfcf4cb3a8ac95d1ae3c8ef4dbdf
Данэль Сибаев <danon.sibaev@gmail.com>
2020-10-22 18:31:17 +0300

    документация, initialState

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Lib
+import Types
+
 
 main :: IO ()
 main = someFunc
+
+list1 :: [[Int]]
+list1 = [[1,0,0],[0,1,0],[0,0,1]]
+
+drawGrid :: PlayGrid -> IO ()
+drawGrid grid = print grid
--- src/Types/Game.hs
+++ src/Types/Game.hs
@@ -1,47 +1,50 @@
 module Types.Game (
   Move,
-  PlayGrid,
-  GameState,
-  mkMove,
-  mkMove1
+  PlayGrid(..),
+  GameState(..),
+  mkMoveFromInt,
+  mkMoveFromStr,
+  initialState
 ) where
 
 import Prelude hiding (Left, Right)
 
 -- | Возможные ходы игрока
 data Move =
-  Up
-    -- ^ выполнить перемещение ячеек вверх
-  | Down
-    -- ^ выполнить перемещение ячеек вниз
-  | Left
-    -- ^ выполнить перемещение ячеек влево
-  | Right
-    -- ^ выполнить перемещение ячеек вправо
+    Up    -- | выполнить перемещение ячеек вверх
+  | Down  -- | выполнить перемещение ячеек вниз
+  | Left  -- | выполнить перемещение ячеек влево
+  | Right -- | выполнить перемещение ячеек вправо
   deriving (Eq,Show,Read)
 
--- | маппинг чисел
-mkMove :: Int -> Maybe Move
-mkMove x
+-- | Получить Move из числа если оно входит в множество [1..4]
+mkMoveFromInt :: Int -> Maybe Move
+mkMoveFromInt x
   | x == 1 = Just (Up)
   | x == 2 = Just (Down)
   | x == 3 = Just (Left)
   | x == 4 = Just (Right)
   | otherwise = Nothing
 
--- | маппинг чисел
-mkMove1 :: String -> Maybe Move
-mkMove1 x
+-- | Получить Move из числа если оно входит в множество [ W, A, S, D ]
+mkMoveFromStr :: String -> Maybe Move
+mkMoveFromStr x
   | x == "w" = Just (Up)
   | x == "a" = Just (Down)
   | x == "s" = Just (Left)
   | x == "d" = Just (Right)
   | otherwise = Nothing
 
--- | Игровое поле
+
+-- | Игровое поле ( 4 * 4 ячейки )
 data PlayGrid = PlayGrid [[Int]] deriving (Eq,Show,Read)
+
+-- | Выигрыш наступает при сборке ячейки со значением 2048
 
 -- | Состояние игры
 data GameState = GameState
   { grid :: PlayGrid }
    deriving (Eq,Show,Read)
+
+initialState :: GameState
+initialState = GameState { grid = PlayGrid [[1], [2]] }
